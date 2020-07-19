70ae94010379f00a1b4a651457094b89fa704a43
HWChia <66512878+xmas21@users.noreply.github.com>
2020-07-19 14:13:48 +0800

    123

--- main.js
+++ main.js
@@ -1,1 +1,38 @@
-console.log("Ian123")
+console.log("Ian123");
+
+// 渲染區域
+// 變數Vue1=new Vue(); 新增一個Vue物件
+// el 指定元素 "選取器"
+// data 資料
+// { 物件名稱: 物件值 }
+var vue1 = new vue1({
+    el: "#vue1"
+    data: {
+        text: "哈囉，我是 Vue !!!",
+        a: 999,
+        b: 3
+    }
+});
+});
+
+
+var vue2 = new Vue({
+    el: "#vue2",
+    data: {
+        aligment: "center",
+        yahoo: "https://tw.yahoo.com/"
+    }
+});
+
+var vue3 = new Vue({
+    el: "#vue3",
+    data: {
+        // 布林值
+        test: true,
+        activity: false
+    }
+});
+
+var vue4 = new Vue({
+    el: "#vue4",
+});
